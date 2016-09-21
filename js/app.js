cb11890d46e3a44d5e8340d13055ff781f07ca1f
aponomarev <aponomarev@dm-matrix.com>
2016-09-21 11:19:54 +0300

    асинхронная загрузка

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 var prototypeApp = angular.module('prototypeApp', ['ngRoute']);
 
-// роутинг
+/* роутинг */
 prototypeApp.config(function($routeProvider) {
 	$routeProvider
 		.when('/', {
@@ -18,18 +18,23 @@ prototypeApp.config(function($routeProvider) {
 			controller  : 'contactController'
 		})
 		;
-	$routeProvider.otherwise({redirectTo: ''}); // редирект в случае ошибочного адреса (изменить на /400)
+	$routeProvider.otherwise({redirectTo: '/'}); // редирект в случае ошибочного адреса (изменить на /400)
 	// $locationProvider.html5Mode(true).hashPrefix('#');  // для замены хешей в путях
 });
+prototypeApp.controller('mainController', function($scope) { $scope.message = 'Everyone come and see how good I look!'; });
+prototypeApp.controller('aboutController', function($scope) { $scope.message = 'Look! I am an about page.'; });
+prototypeApp.controller('contactController', function($scope) { $scope.message = 'Contact us! JK. This is just a demo.'; });
 
-// amitgharat.wordpress.com/2013/02/03/an-approach-to-use-jquery-plugins-with-angularjs/
+/* amitgharat.wordpress.com/2013/02/03/an-approach-to-use-jquery-plugins-with-angularjs/ */
 prototypeApp.directive('jq.draggable', function() {
     return {
     	restrict: 'A',
     	link: function() { $('.jq-draggable').draggable( { grid: [ 50, 20 ], cursor: 'crosshair' } ); }
     };
 });
-
-prototypeApp.controller('mainController', function($scope) { $scope.message = 'Everyone come and see how good I look!'; });
-prototypeApp.controller('aboutController', function($scope) { $scope.message = 'Look! I am an about page.'; });
-prototypeApp.controller('contactController', function($scope) { $scope.message = 'Contact us! JK. This is just a demo.'; });
+prototypeApp.directive('jq.indicator', function() {
+    return {
+    	restrict: 'A',
+    	link: function() { click_indicator('.jq-indicator','active'); }
+    };
+});
