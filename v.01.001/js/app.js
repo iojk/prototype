d5bef2b950f10c82de1aebf096877e6ab230670f
aponomarev <aponomarev@dm-matrix.com>
2016-09-21 15:14:00 +0300

    переключалка версионности

---
+++ v.01.001/js/app.js
@@ -0,0 +1,40 @@
+// script.js
+
+var prototypeApp = angular.module('prototypeApp', ['ngRoute']);
+
+/* роутинг */
+prototypeApp.config(function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl : 'page/home.html',
+			controller  : 'mainController'
+		})
+		.when('/about', {
+			templateUrl : 'page/about.html',
+			controller  : 'aboutController'
+		})
+		.when('/contact', {
+			templateUrl : 'page/contact.html',
+			controller  : 'contactController'
+		})
+		;
+	$routeProvider.otherwise({redirectTo: '/'}); // редирект в случае ошибочного адреса (изменить на /400)
+	// $locationProvider.html5Mode(true).hashPrefix('#');  // для замены хешей в путях
+});
+prototypeApp.controller('mainController', function($scope) { $scope.message = 'Everyone come and see how good I look!'; });
+prototypeApp.controller('aboutController', function($scope) { $scope.message = 'Look! I am an about page.'; });
+prototypeApp.controller('contactController', function($scope) { $scope.message = 'Contact us! JK. This is just a demo.'; });
+
+/* amitgharat.wordpress.com/2013/02/03/an-approach-to-use-jquery-plugins-with-angularjs/ */
+prototypeApp.directive('jq.draggable', function() {
+    return {
+    	restrict: 'A',
+    	link: function() { $('.jq-draggable').draggable( { grid: [ 50, 20 ], cursor: 'crosshair' } ); }
+    };
+});
+prototypeApp.directive('jq.indicator', function() {
+    return {
+    	restrict: 'A',
+    	link: function() { click_indicator('.jq-indicator','active'); }
+    };
+});
