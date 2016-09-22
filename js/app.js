5fb787bb8433727f1d248368d140de4a1f1e23da
aponomarev <aponomarev@dm-matrix.com>
2016-09-22 14:40:04 +0300

    корректировка роутинга ...

--- js/app.js
+++ js/app.js
@@ -3,10 +3,8 @@
 var prototypeApp = angular.module('prototypeApp', ['ngRoute']);
 
 /* роутинг */
-
 prototypeApp.config(['$routeProvider', '$locationProvider',
 	function($routeProvider, $locationProvider) {
-		//$locationProvider.html5Mode(true); устранение хешей в урлах
 		$routeProvider
 			.when('/', {
 				templateUrl : 'page/home.html',
@@ -28,6 +26,8 @@ prototypeApp.config(['$routeProvider', '$locationProvider',
 prototypeApp.controller('mainController', function($scope) { $scope.message = 'Everyone come and see how good I look!'; });
 prototypeApp.controller('aboutController', function($scope) { $scope.message = 'Look! I am an about page.'; });
 prototypeApp.controller('contactController', function($scope) { $scope.message = 'Contact us! JK. This is just a demo.'; });
+
+
 
 /* amitgharat.wordpress.com/2013/02/03/an-approach-to-use-jquery-plugins-with-angularjs/ */
 prototypeApp.directive('jq.draggable', function() {
