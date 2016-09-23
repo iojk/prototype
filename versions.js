27fcbca3280a82e6a8094bcb85aa5787f2eb1c54
aponomarev <aponomarev@dm-matrix.com>
2016-09-23 12:50:52 +0300

    навешивание коротких ссылок при переходе внутри версии #2

--- versions.js
+++ versions.js
@@ -4,8 +4,6 @@ var window_location_href = window.location.href; // полный адрес ак
 var link_page = window_location_href.match(/\/#\/.*$/).toString().replace(/\/#/,''); // короткий адрес активного экрана
 var version_ref = document.referrer.replace(/.*\/prototype\//,'').replace(/\/.*/,'');
 
-//alert(version_ref);
-
 /* корректировка ссылок в выпадающем меню выбора версий пототипа */
 $('a[href*="#/"]').each(function(){
 	$(this).click(function(){
@@ -16,14 +14,15 @@ $('a[href*="#/"]').each(function(){
 	});
 });
 
-/* выбор активной версии прототипа */
 $('#prototype-version-select option').each(function(){
 	var version = $(this).attr('value').replace(/\.\.\//,'').replace(/\/#\//,'');
+	/* выбор активной версии прототипа */
 	if ( window_location_href.indexOf(version) != -1 ) {
 		$(this).attr('selected','selected');
 	} // если версия открытого экрана соответствует пункту меню
+	/* навешивание коротких ссылок при переходе внутри версии */
 	if ( version == version_ref ) {
 		$(this).attr('value',$(this).attr('value').replace(/\/#\/.*/,'/#'+link_page));
-	} // навешивание коротких ссылок при переходе внутри версии
+	}
 });
 
