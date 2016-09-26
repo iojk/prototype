fd6017e39346811bf7c0ed8122da90c993d89fcc
aponomarev <aponomarev@dm-matrix.com>
2016-09-26 15:56:31 +0300

    добавляем имя цветовой модели от предыдущей версии просмотра к статичным ссылкам

--- versions.js
+++ versions.js
@@ -4,17 +4,20 @@ var window_location_href = window.location.href; // полный адрес ак
 var link_page = window_location_href.match(/\/#\/.*$/).toString().replace(/\/#/,'').replace(/\?.*/,''); // короткий адрес активного экрана
 var version_ref = document.referrer.replace(/.*\/prototype\//,'').replace(/\/.*/,'');
 var color_model = $('body').attr('class').match(/\S*colors-\S*\s/).toString().replace(/\s/g,''); // имя цветовой модели
-//var color_model_ref = window.location.hash.toString().match(/colors-[a-z]+/);
-
-alert(color_model);
+var color_model_ref = window.location.hash.match(/colors-[a-z]+/);
 
 /* смена дефолтной цветовой модели при переходе из другой версии */
-//if ( color_model_ref != 'null' ) {
-//	$('body').attr('class',$('body').attr('class').replace(/colors-[a-z]*/g,'')).addClass(color_model_ref);
-//} // добавляем имя цветовой модели от предыдущей версии просмотра
+if ( color_model_ref != null ) {
+	$('body').attr('class',$('body').attr('class').replace(/colors-[a-z]*/g,'')).addClass(color_model_ref.toString());
+} // добавляем имя цветовой модели от предыдущей версии просмотра
 
 /* корректировка ссылок в выпадающем меню выбора версий пототипа */
 $('a[href*="#/"]').each(function(){
+	if ( color_model_ref != null ) {
+    	$(this).attr('href',$(this).attr('href').replace(/$/,'?'+color_model_ref.toString()));
+    } else {
+    	$(this).attr('href',$(this).attr('href').replace(/$/,'?'+color_model));
+    }  // добавляем имя цветовой модели от предыдущей версии просмотра
 	$(this).click(function(){
 		var link = $(this).attr('href');
 		$('#prototype-version-select option').each(function(){
