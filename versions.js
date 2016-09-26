c14c10873722394d841fef80c288a1b27d0b7049
aponomarev <aponomarev@dm-matrix.com>
2016-09-26 17:32:26 +0300

    переключении цветовой модели в панели версий

--- versions.js
+++ versions.js
@@ -5,16 +5,17 @@ var link_page = window_location_href.match(/\/#\/.*$/).toString().replace(/\/#/,
 var version_ref = document.referrer.replace(/.*\/prototype\//,'').replace(/\/.*/,'');
 var color_model = $('body').attr('class').match(/\S*colors-\S*\s/).toString().replace(/\s/g,''); // имя цветовой модели
 var color_model_ref = window.location.hash.match(/colors-[a-z]+/);
+var color_ref = color_model_ref.toString();
 
 /* смена дефолтной цветовой модели при переходе из другой версии */
 if ( color_model_ref != null ) {
-	$('body').attr('class',$('body').attr('class').replace(/colors-[a-z]*/g,'')).addClass(color_model_ref.toString());
+	$('body').attr('class',$('body').attr('class').replace(/colors-[a-z]*/g,'')).addClass(color_ref);
 } // добавляем имя цветовой модели от предыдущей версии просмотра
 
 /* корректировка ссылок в выпадающем меню выбора версий пототипа */
 $('a[href*="#/"]').each(function(){
 	if ( color_model_ref != null ) {
-    	$(this).attr('href',$(this).attr('href').replace(/\?.*|$/,'?'+color_model_ref.toString()));
+    	$(this).attr('href',$(this).attr('href').replace(/\?.*|$/,'?'+color_ref));
     } else {
     	$(this).attr('href',$(this).attr('href').replace(/\?.*|$/,'?'+color_model));
     }  // добавляем имя цветовой модели от предыдущей версии просмотра
@@ -36,7 +37,7 @@ $('#prototype-version-select option').each(function(){
 	// навешивание коротких ссылок при переходе внутри версии
 	if ( version == version_ref || version_ref == '' ) {
 		if ( color_model_ref != null ) {
-        	$(this).attr('value',$(this).attr('value').replace(/\/#\/.*/,'/#'+link_page+'?'+color_model_ref.toString()));
+        	$(this).attr('value',$(this).attr('value').replace(/\/#\/.*/,'/#'+link_page+'?'+color_ref));
         } else {
         	$(this).attr('value',$(this).attr('value').replace(/\/#\/.*/,'/#'+link_page+'?'+color_model));
         }
