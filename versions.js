253426fe6862f6e0e9168009e9fcce7e562b8114
aponomarev <aponomarev@dm-matrix.com>
2016-09-29 11:10:45 +0300

    правильная переключалка нижней панели - правки

--- versions.js
+++ versions.js
@@ -5,14 +5,13 @@ var address_location = window.location.href; // полный адрес экра
 var address_version = address_location.replace(/\/#.*/,'').match(/\/[^/]*$/).toString().replace(/[/]/,''); // имя версии
 var address_hash = address_location.match(/\/#.*/).toString().replace(/#[^/].*/,'') // адрес без учета якоря
 var address_short = address_hash.replace(/\/#/,'').replace(/\?.+/,''); // короткий адрес экрана
-var address_search_r = address_hash.match(/\?[^#]*/);
-var address_search = '';
 var address_colors_r = address_hash.match(/colors[-][^&]*/);
 var address_colors = ''; // имя цветовой модели
 // переменные из тела документа
 var doc_version = $('base').attr('href').replace(/..\//,'').replace(/\/.*/,''); // имя версии из заголовка документа
 var doc_colors = $('body').attr('class').match(/colors-[^ ]*/).toString(); // имя цветовой модели
 
+/* начальная установка параметров */
 $('a[href*="#/"]').each(function(){
 	$(this).click(function(){
 		address_short = $(this).attr('href').replace(/#/,'').replace(/\?.*/,'');
@@ -20,21 +19,26 @@ $('a[href*="#/"]').each(function(){
 });
 
 $('#prototype-color-select select').change(function(){
-	address_colors = $(this).val()
+	address_colors = $(this).val();
+	doc_colors = address_colors;
 	window.location = '../' + address_version + '/#' + address_short + '?' + address_colors;
 	colorChange('body',address_colors);
-	linkChange('a[href*="#/"]',/$/,address_colors);
+	linkChange('a[href*="#/"]',/(\?.*)|$/,address_colors);
     versionChange('#prototype-version-select select',address_colors);
 });
 
 if (address_colors_r) { // цвет объявлен в запросе адресной строки
-	address_search = address_search_r.toString();
 	address_colors = address_colors_r.toString();
+	doc_colors = address_colors;
     colorChange('body',address_colors);
-    linkChange('a[href*="#/"]',/$/,address_colors);
+    linkChange('a[href*="#/"]',/(\?.*)|$/,address_colors);
     versionChange('#prototype-version-select select',address_colors);
 } else {
-	linkChange('a[href*="#/"]',/$/,doc_colors);
+	address_colors = doc_colors;
+	$('#prototype-color-select select option').removeAttr('selected');
+	$('#prototype-color-select select option[value="'+ doc_colors +'"]').attr('selected','selected');
+	window.location = '../' + address_version + '/#' + address_short + '?' + doc_colors;
+	linkChange('a[href*="#/"]',/(\?.*)|$/,doc_colors);
 	versionChange('#prototype-version-select select',doc_colors);
 }
 
@@ -62,7 +66,7 @@ function colorChange(selector,modifier) {
 	$(selector).find('option').each(function(){
 		var color = $(this).attr('value');
 		if ( address_location.indexOf(color) != -1 ) {
-        	$(this).attr('selected','selected'); // выбор активной цветовой модели прототипа
+			$(this).attr('selected','selected'); // выбор активной цветовой модели прототипа
         } // если цветовая модель открытого экрана соответствует пункту меню
 	});
 	$(selector).attr('class',$(selector).attr('class').replace(/colors-[^ ]*/g,'')).addClass(modifier);
