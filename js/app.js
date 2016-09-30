f23421f065a2c482db2141c693c80d768a5db269
aponomarev <aponomarev@dm-matrix.com>
2016-09-30 12:52:55 +0300

    подгрузка скриптов через get

--- js/app.js
+++ js/app.js
@@ -1,23 +1,16 @@
 // app.js
 
-var prototypeApp = angular.module('prototypeApp', ['ngRoute']);
+$.ajax({type:'GET',cache:true,dataType:'script',url:'../js/get/html-doc-head.js'}); // добавление метатегов в заголовок html-документа
+$('body').append('<footer class="prototype-versions container-fluid navbar-fixed-bottom" ng-include="\'../versions.html\'"></footer>');
 
 /* роутинг */
+var prototypeApp = angular.module('prototypeApp', ['ngRoute']);
 prototypeApp.config(['$routeProvider', '$locationProvider',
 	function($routeProvider, $locationProvider) {
 		$routeProvider
-			.when('/', {
-				templateUrl : 'page/home.html',
-				controller  : 'mainController'
-			})
-			.when('/about', {
-				templateUrl : 'page/about.html',
-				controller  : 'aboutController'
-			})
-			.when('/contact', {
-				templateUrl : 'page/contact.html',
-				controller  : 'contactController'
-			})
+			.when('/', { templateUrl : 'page/home.html', controller  : 'mainController' })
+			.when('/about', { templateUrl : 'page/about.html', controller  : 'aboutController' })
+			.when('/contact', { templateUrl : 'page/contact.html', controller  : 'contactController' })
 			.otherwise({redirectTo: '/'})
 		;
 	}
@@ -26,35 +19,15 @@ prototypeApp.controller('mainController', function($scope) { $scope.message = 'E
 prototypeApp.controller('aboutController', function($scope) { $scope.message = 'Look! I am an about page.'; });
 prototypeApp.controller('contactController', function($scope) { $scope.message = 'Contact us! JK. This is just a demo.'; });
 
-/* amitgharat.wordpress.com/2013/02/03/an-approach-to-use-jquery-plugins-with-angularjs/ */
+/* amitgharat.wordpress.com/2013/02/03/an-approach-to-use-jquery-plugins-with-angularjs */
 prototypeApp.directive('jq.draggable', function() {
-    return {
-    	restrict: 'A',
-    	link: function() { $('.jq-draggable').draggable( { grid: [ 50, 20 ], cursor: 'crosshair' } ); }
-    };
+    return { restrict: 'A', link: function() { $('.jq-draggable').draggable( { grid: [ 50, 20 ], cursor: 'crosshair' } ); } };
 });
+/* установка модификатора родителю по клику на дочернем элементе */
 prototypeApp.directive('jq.indicator', function() {
-    return {
-    	restrict: 'A',
-    	link: function() { click_indicator('.jq-indicator','active'); }
-    };
+    return { restrict: 'A', link: function() { click_indicator('.jq-indicator','active'); } };
 });
+/* ленивая подгрузка картинок */
 prototypeApp.directive('jq.lazyload', function() {
-    return {
-    	restrict: 'A',
-    	link: function() { $('.jq-lazyload').lazyload( { effect: 'fadeIn' } ); }
-    };
+    return { restrict: 'A', link: function() { $('.jq-lazyload').lazyload( { effect: 'fadeIn', event: 'scroll' } ); } };
 });
-
-$('head').prepend('\
-	<meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible"><!-- эта meta должна быть первой, иначе не сработает! -->\
-	<meta content="width=device-width, initial-scale=1.0" name="viewport"><!-- frontender.com.ua/mobile-web/wtf-viewport -->\
-	<meta content="true" name="HandheldFriendly"><!-- frontender.com.ua/mobile-web/useful-mobile-head-elements -->\
-	<meta charset="utf-8">\
-	<title>Prototype</title>\
-	<link rel="shortcut icon" href="../favicon.ico">\
-	<link rel="icon" href="../favicon.ico">'
-); // подключение метатегов, иконок
-$('body').append('\
-	<footer class="prototype-versions container-fluid navbar-fixed-bottom" ng-include="\'../versions.html\'"></footer>'
-); // автоподключение футера
---
+++ js/get/html-doc-head.js
@@ -0,0 +1,24 @@
+/* подключение метатегов, скриптов, иконок */
+
+$('head').prepend('\
+	<meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible"><!-- эта meta должна быть первой, иначе не сработает! -->\
+	<meta content="width=device-width, initial-scale=1.0" name="viewport"><!-- frontender.com.ua/mobile-web/wtf-viewport -->\
+	<meta content="true" name="HandheldFriendly"><!-- frontender.com.ua/mobile-web/useful-mobile-head-elements -->\
+	<meta charset="utf-8">\
+	<title>Prototype</title>\
+	<link rel="shortcut icon" href="../favicon.ico">\
+	<link rel="icon" href="../favicon.ico">\
+	<!--[if lt IE 9]>\
+    <script defer src="//html5shiv/3.7.3/html5shiv.min.js"></script>\
+    <![endif]-->\
+	<script defer src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>\
+	<script defer src="//cdnjs.cloudflare.com/ajax/libs/less.js/2.7.1/less.min.js"></script>\
+<!-- jQuery -->\
+	<script defer src="//code.jquery.com/ui/1.12.0/jquery-ui.min.js"></script>\
+	<script defer src="//cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/pace.min.js"></script><!-- индикатор загрузки github.hubspot.com/pace/docs/welcome/ -->\
+	<script defer src="../js/jquery/functions.js"></script>\
+<!-- Bootstrap -->\
+	<script defer src="//cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js"></script><!-- требуется для бутстрапа -->\
+	<script defer src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/js/bootstrap.min.js"></script>\
+<!-- Application -->\
+	<link rel="stylesheet/less" href="style/app.less">');
