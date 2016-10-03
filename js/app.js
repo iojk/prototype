d77db44b96661cad6efe4bc8218475b68d424e9d
aoponomarev <aoponomarev@ya.ru>
2016-10-03 15:40:42 +0300

    подгрузка lazyload

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 // app.js
 
 $.ajax({type:'GET',cache:true,dataType:'script',url:'../get/html/html-doc-head.js'}); // добавление метатегов и скриптов в заголовок html-документа
+$.ajax({type:'GET',cache:true,dataType:'script',url:'//cdnjs.cloudflare.com/ajax/libs/jquery.lazyload/1.9.1/jquery.lazyload.min.js'});
 $('body').append('<footer class="prototype-versions container-fluid navbar-fixed-bottom" ng-include="\'../versions.html\'"></footer>');
 
 /* роутинг */
